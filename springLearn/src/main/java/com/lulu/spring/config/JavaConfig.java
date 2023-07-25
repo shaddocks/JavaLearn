f94a9bc0cc810653316ba3a4a4ccfe232e127454
shaddock <m15926539316@163.com>
2023-07-25 21:02:02 +0800

    aop

--- springLearn/src/main/java/com/lulu/spring/config/JavaConfig.java
+++ springLearn/src/main/java/com/lulu/spring/config/JavaConfig.java
@@ -1,11 +1,30 @@
 package com.lulu.spring.config;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.*;
 
 @Configuration
 @ComponentScan(value = {"com.lulu.spring"})
 @PropertySource("classpath:/jdbc.properties")
+@EnableAspectJAutoProxy
 public class JavaConfig {
+    @Value("${prop.userName}")
+    String userName;
+    @Value("${prop.password}")
+    String password;
+    @Value("${prop.url}")
+    String url;
+    @Value("${prop.driverClass}")
+    String driver;
+
+    @Bean
+    public DruidDataSource druidDataSource() {
+        DruidDataSource druidDataSource = new DruidDataSource();
+        druidDataSource.setUsername(userName);
+        druidDataSource.setPassword(password);
+        druidDataSource.setUrl(url);
+        druidDataSource.setDriverClassName(driver);
+        return druidDataSource;
+    }
 }
